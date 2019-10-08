e3a0333092cf2ebd026894bb5d777a8391546811
Ryan Carey <rcarey@dataxu.com>
2019-10-08 15:20:58 -0400

    fix duplicate extension test name

--- test/extension_test.rb
+++ test/extension_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ExtensionTest < Test::Unit::TestCase
   
-  def test_should_know_attributes
+  def test_should_know_attributes_type
     document_file = example_file('document_with_one_inline_ad.xml')
     document = VAST::Document.parse!(document_file)
     extension = VAST::Extension.new(document.at('Extension'))
@@ -10,7 +10,7 @@ class ExtensionTest < Test::Unit::TestCase
     assert_equal "DART", extension.type
   end
   
-  def test_should_know_attributes
+  def test_should_know_attributes_xml
     document_file = example_file('document_with_one_inline_ad.xml')
     document = VAST::Document.parse!(document_file)
     extension = VAST::Extension.new(document.at('Extension'))
